9b9128e7af43c284b53b8aebfabdcf2f5e65ff6f
Pablo <phernandezm07@gmail.com>
2024-12-31 04:00:44 -0500

    fix: Extract plugin keymaps from keymap.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -17,6 +17,21 @@ return {
           }
         }
       }
+
+      vim.keymap.set("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+      vim.keymap.set("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+      vim.keymap.set("n", "<leader>bo", "<CMD>BufferLineCloseOthers<CR>", { desc = "Close others" })
+      vim.keymap.set("n", "<leader>bl", "<CMD>BufferLineCloseRight<CR>", { desc = "Close right" })
+      vim.keymap.set("n", "<leader>bh", "<CMD>BufferLineCloseLeft<CR>", { desc = "Close left" })
+      vim.keymap.set("n", "<leader>ba",
+        function()
+          for _, e in ipairs(require("bufferline").get_elements().elements) do
+            vim.schedule(function()
+              vim.cmd("bd " .. e.id)
+            end)
+          end
+        end,
+        { desc = "Close all buffers" })
     end
   },
 }
--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -13,5 +13,7 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.keymap.set("n", "<leader>DT", "<CMD>DBUIToggle<CR>", { desc = "Toggle UI" })
+    vim.keymap.set("n", "<leader>DF", "<CMD>DBUIFindBuffer<CR>", { desc = "Find buffer" })
   end,
 }
