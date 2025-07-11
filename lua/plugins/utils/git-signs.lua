f8f9c10444fb5b80b449113943defda24e33acb5
pablohernandezm <phernandezm07@gmail.com>
2025-07-11 06:06:03 -0500

    feat: Git integrations

---
+++ lua/plugins/utils/git-signs.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		keys = {
+			{ "<leader>gp", ":Gitsigns preview_hunk<CR>", desc = "Git preview hunk" },
+			{ "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", desc = "Git toggle current line blame" },
+		},
+		opts = {},
+	},
+}
