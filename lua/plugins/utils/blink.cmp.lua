7c690036725d0a963ea55187d7b24a22dc93db84
pablohernandezm <phernandezm07@gmail.com>
2025-07-11 06:04:21 -0500

    feat: blink.cmp

---
+++ lua/plugins/utils/blink.cmp.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"saghen/blink.cmp",
+
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "1.*",
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+
+			signature = {
+				enabled = true,
+			},
+		},
+	},
+}
