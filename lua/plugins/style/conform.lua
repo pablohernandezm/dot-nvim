e3f2b076f7641ccf143ade575e25f8af420b64f5
pablohernandezm <phernandezm07@gmail.com>
2025-07-14 23:32:18 -0500

    feat(format): added deno fmt for supported files

--- lua/plugins/style/conform.lua
+++ lua/plugins/style/conform.lua
@@ -7,7 +7,7 @@ return {
 
 		keys = {
 			{
-				"<leader>f",
+				"<leader>bf",
 				function()
 					require("conform").format({ async = true, lsp_format = "fallback" })
 				end,
@@ -15,16 +15,43 @@ return {
 				desc = "Format buffer",
 			},
 		},
-		opts = function()
-			require("conform").setup({
+
+		config = function()
+			---@module "conform"
+			---@type conform.setupOpts
+			local options = {
 				formatters_by_ft = {
-					typst = { "typstyle" },
-					lua = { "stylua" },
-					sql = { "pg_format" },
+					-- General
 					nix = { "alejandra" },
-					rust = { "rustfmt" },
+					lua = { "stylua" },
+					rs = { "rustfmt" },
+					sql = { "pg_format", "deno_fmt" },
+					astro = { "deno_fmt" },
+					html = { "deno_fmt" },
+					ipynb = { "deno_fmt" },
+					javascript = { "deno_fmt" },
+					javascriptreact = { "deno_fmt" },
+					markdown = { "deno_fmt" },
+					njk = { "deno_fmt" },
+					svelte = { "deno_fmt" },
+					typescript = { "deno_fmt" },
+					typescriptreact = { "deno_fmt" },
+					vue = { "deno_fmt" },
+					vto = { "deno_fmt" },
+					yaml = { "deno_fmt" },
+					typst = { "typstyle" },
+					-- CSS variants
+					css = { "deno_fmt" },
+					less = { "deno_fmt" },
+					sass = { "deno_fmt" },
+					scss = { "deno_fmt" },
+					-- Json variants
+					json = { "deno_fmt" },
+					jsonc = { "deno_fmt" },
 				},
-			})
+			}
+
+			require("conform").setup(options)
 
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				pattern = "*",
